292a2339bb714c222b4ee1d673d5992b3d5775a7
Adam O'Brien <obri0056@algonquinlive.com>
2020-10-09 21:16:49 -0400

    Step 6 Layout

--- app/src/main/java/com/example/lab3_1/ProfileActivity.java
+++ app/src/main/java/com/example/lab3_1/ProfileActivity.java
@@ -2,13 +2,41 @@ package com.example.lab3_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    ImageButton mImageButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        mImageButton = findViewById(R.id.takePhoto);
+        mImageButton.setOnClickListener(tp -> dispatchTakePictureIntent() );
+        Log.e(ACTIVITY_NAME,"In function:"+ ACTIVITY_NAME);
+
+
+        }
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);}}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+        }
     }
+
 }
