939d225104403f1cbd820d24af2b206495f070ec
Adam O'Brien <obri0056@algonquinlive.com>
2020-10-22 21:08:37 -0400

    Lab4

--- app/src/main/java/com/example/lab3_1/MessageActivity.java
+++ app/src/main/java/com/example/lab3_1/MessageActivity.java
@@ -1,4 +1,70 @@
+
 package com.example.lab3_1;
 
-public class MessageActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MessageActivity extends Activity {
+
+    myListAdapter myListAdapter = new myListAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+        Button send = findViewById(R.id.sendButton);
+        Button receive = findViewById(R.id.receiveButton);
+        final android.widget.ListView myList = (android.widget.ListView) findViewById(R.id.listViewLayout);
+        myList.setAdapter( (ListAdapter) myListAdapter );
+
+        final ArrayList<String> list = new ArrayList<String>();
+
+        send.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    public class myListAdapter extends BaseAdapter{
+        private ArrayList<String> elements = new ArrayList<>();
+        private  LayoutInflater inflater = getLayoutInflater();
+
+        @Override
+        public int getCount() {
+            return elements.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View newView = inflater.inflate(R.layout.listview, parent ,false);
+            return newView;
+        }
+    }
+
 }
+
+
--- app/src/main/java/com/example/lab3_1/ProfileActivity.java
+++ app/src/main/java/com/example/lab3_1/ProfileActivity.java
@@ -1,15 +1,19 @@
+
+
+
 package com.example.lab3_1;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.os.Bundle;
+        import android.provider.MediaStore;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageButton;
 
 public class ProfileActivity extends AppCompatActivity {
     ImageButton mImageButton;
@@ -20,16 +24,37 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         mImageButton = findViewById(R.id.takePhoto);
+        Button messageButton = findViewById(R.id.Messages);
+        Intent nextPage = new Intent(this, MessageActivity.class);
+        messageButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(nextPage);
+            }
+        });
         mImageButton.setOnClickListener(tp -> dispatchTakePictureIntent() );
         Log.e(ACTIVITY_NAME,"In function:"+ "onCreate");
-        Log.e(ACTIVITY_NAME,"In function:"+ "onStart");
-        Log.e(ACTIVITY_NAME,"In function:"+ "onPause");
-        Log.e(ACTIVITY_NAME,"In function:"+ "onStop");
-        Log.e(ACTIVITY_NAME,"In function:"+ "onDestroy");
-        Log.e(ACTIVITY_NAME,"In function:"+ "onActivityResult");
 
     }
 
+    protected void onStart(){
+        super.onStart();
+        Log.e(ACTIVITY_NAME,"In function:"+ "onStart");
+    }
+    protected void onPause(){
+        super.onPause();
+        Log.e(ACTIVITY_NAME,"In function:"+ "onPause");
+
+    }
+    protected void onStop(){
+        super.onStop();
+        Log.e(ACTIVITY_NAME,"In function:"+ "onStop");
+
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME,"In function:"+ "onDestroy");
+
+    }
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -43,6 +68,7 @@ public class ProfileActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageButton.setImageBitmap(imageBitmap);
         }
+        Log.e(ACTIVITY_NAME,"In function:"+ "onActivityResult");
     }
 
 }
