bd1764fd27ac8d4f773bdc553acb6d29bfd6acfb
Adam O'Brien <obri0056@algonquinlive.com>
2020-11-11 21:42:20 -0500

    Lab6

--- app/src/main/java/com/example/lab3_1/WeatherForcast.java
+++ app/src/main/java/com/example/lab3_1/WeatherForcast.java
@@ -8,10 +8,13 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -22,12 +25,21 @@ public class WeatherForcast extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forcast);
         ForecastQuery  req = new ForecastQuery ();
-        req.execute("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric");
+        req.execute("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric",
+                "http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389"
+                );
 
     }
 
 
  private class ForecastQuery extends AsyncTask< String, Integer, String> {
+
+     TextView tempSet;
+     TextView minSet;
+     TextView maxSet;
+     TextView uvSet;
+     double uvRating;
+     String maxTemp, minTemp, curTemp;
 
     public String doInBackground(String ... args)
     {
@@ -62,39 +74,47 @@ public class WeatherForcast extends AppCompatActivity {
                 if(eventType == XmlPullParser.START_TAG)
                 {
                     //If you get here, then you are pointing at a start tag
-                    if(xpp.getName().equals("Temperature"))
+                    if(xpp.getName().equals("temperature"))
                     {
                         //If you get here, then you are pointing to a <Weather> start tag
-                        String tempValue = xpp.getAttributeValue(null,    "value");
-                        TextView tempSet = findViewById(R.id.currentTemp);
-                        tempSet.setText(tempValue);
-                        String minTempValue = xpp.getAttributeValue(null, "min");
-                        TextView minSet = findViewById(R.id.minTemp);
-                        minSet.setText(minTempValue);
-                        String maxTempValue = xpp.getAttributeValue(null, "max");
-                        TextView maxSet = findViewById(R.id.maxTemp);
-                        maxSet.setText(maxTempValue);
+                       curTemp= xpp.getAttributeValue(null,    "value");
+
+                         minTemp = xpp.getAttributeValue(null, "min");
+
+                         maxTemp = xpp.getAttributeValue(null, "max");
+
 
 
                     }
 
-                    else if(xpp.getName().equals("AMessage"))
-                    {
-                        parameter = xpp.getAttributeValue(null, "message"); // this will run for <AMessage message="parameter" >
-                    }
-                    else if(xpp.getName().equals("Weather"))
-                    {
-                        parameter = xpp.getAttributeValue(null, "outlook"); //this will run for <Weather outlook="parameter"
-                        parameter = xpp.getAttributeValue(null, "windy"); //this will run for <Weather windy="paramter"  >
-                    }
-                    else if(xpp.getName().equals("Temperature"))
-                    {
-                        xpp.next(); //move the pointer from the opening tag to the TEXT event
-                        parameter = xpp.getText(); // this will return  20
-                    }
                 }
                 eventType = xpp.next(); //move to the next xml event and store it in a variable
             }
+
+
+
+
+            //done xml
+            //create a URL object of what server to contact:
+             url = new URL(args[1]);
+
+            //open the connection
+             urlConnection = (HttpURLConnection) url.openConnection();
+
+            //wait for data:
+             response = urlConnection.getInputStream();
+
+    // json is UTF-8 by default
+            BufferedReader reader = new BufferedReader(new InputStreamReader(response, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null)
+            {
+                sb.append(line + "\n");
+            }
+            String result = sb.toString();
+            JSONObject resp = new JSONObject((result));//convet string to jsonobject
+            uvRating = resp.getDouble("value");
 
 
         }
@@ -113,7 +133,17 @@ public class WeatherForcast extends AppCompatActivity {
     }
     //Type3
     public void onPostExecute(String fromDoInBackground)
-    {
+    {     tempSet = findViewById(R.id.currentTemp);
+        tempSet.setText("Current Temp: "+ curTemp+" ");
+        minSet = findViewById(R.id.minTemp);
+        minSet.setText("Min: "+minTemp+" ");
+        maxSet = findViewById(R.id.maxTemp);
+        maxSet.setText("Max: "+maxTemp+" ");
+        uvSet = findViewById(R.id.UV);
+      String  uvR=String.valueOf( uvRating);
+        uvSet.setText("UV: " +uvR+" ");
+
+
         Log.i("HTTP", fromDoInBackground);
     }
 }
